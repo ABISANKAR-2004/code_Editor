126bebb706e46a12ac48bde7bab0b5e9e8576ed8
ABISANKAR-2004 <abiabisankar2004@gmail.com>
2025-12-09 19:24:34 +0530

    built  commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 import Editor from "@monaco-editor/react";
 import { v4 as uuid } from "uuid";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://code-editor-ph8n.onrender.com");
 
 const App = () => {
   const [joined, setJoined] = useState(false);
